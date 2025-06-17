dc183bb3f8396b921ea1cd5b4475e61bae5ee2ac
MANGALISO MADUNA <mangimaduna.mangaliso@gmail.com>
2025-06-17 11:36:42 +0200

    signal setup to create profile automatically

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 # Create your models here.
 class User(AbstractUser):
@@ -43,4 +44,14 @@ class UserProfile(models.Model):
             self.full_name = self.user.full_name
   
         super(UserProfile, self).save(*args, **kwargs)
-    
+
+# creating a signal to create UserProfile when User is created
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
